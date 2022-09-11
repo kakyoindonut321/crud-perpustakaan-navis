48302d1e0620939262473c3892a5f11e9ad3de86
kakyoindonut321 <forbiddenikki@gmail.com>
2022-09-11 23:21:15 +0700

    commit pertama, import dari repo sebelumnya, tapi yang ini khusus punya saya

---
+++ variable.php
@@ -0,0 +1,22 @@
+<?php
+function variable() {
+    $db_function[0] = "localhost";
+    $db_function[1] = "root";
+    $db_function[2] = "";
+    $db_function[3] = "library_db";
+    return $db_function;
+}
+
+function but() {
+    echo "
+    <footer class='my-5 pt-5 text-muted text-center text-small'>
+    <p class='mb-1'>&copy; 2017–2022 Copyright© Muhammad Rifqi Navis</p>
+    <ul class='list-inline'>
+      <li class='list-inline-item'><a href='#'>Privacy</a></li>
+      <li class='list-inline-item'><a href='#'>Terms</a></li>
+      <li class='list-inline-item'><a href='#'>Support</a></li>
+    </ul>
+  </footer>
+    ";
+}
+?>
